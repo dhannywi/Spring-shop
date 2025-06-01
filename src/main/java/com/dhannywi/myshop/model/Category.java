b3297d53979f2c20250e4f29f28b589b08c33048
Dhanny Indrakusuma <dwindrakusuma@gmail.com>
2025-05-31 22:05:23 -0500

    create Category-Product relationship

--- src/main/java/com/dhannywi/myshop/model/Category.java
+++ src/main/java/com/dhannywi/myshop/model/Category.java
@@ -1,10 +1,23 @@
 package com.dhannywi.myshop.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
