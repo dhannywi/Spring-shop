56beff05513faf867e77229d30f16517ac1276ee
Dhanny Indrakusuma <dwindrakusuma@gmail.com>
2025-05-31 21:04:41 -0500

    create model classes

---
+++ src/main/java/com/dhannywi/myshop/model/Product.java
@@ -0,0 +1,32 @@
+package com.dhannywi.myshop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    private  Category category;
+
+    // when a product is deleted, all associated images will be deleted too
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+}
